2dc89f728c07e873c17d985eecee13a1bfe7a4f4
Mark Washeim <blueprint@poetaster.de>
2023-07-02 22:41:19 +0200

    Fix: missed instruments

---
+++ src/instrument.js
@@ -0,0 +1,42 @@
+import * as Tone from 'tone';
+
+export default function getAudioModel(scene) {
+    const conga = new Tone.MembraneSynth({
+			pitchDecay: 0.008,
+			octaves: 2,
+			envelope: {
+				attack: 0.0006,
+				decay: 0.5,
+				sustain: 0
+			}
+    });
+    conga.toDestination();
+
+    const congaPart = new Tone.Sequence(((time, pitch) => {
+		conga.triggerAttack(pitch, time, Math.random()*0.5 + 0.5);
+    }), ["G3", "C4", "C4", "C4"], "4n");
+
+    const bell = new Tone.MetalSynth({
+            harmonicity: 12,           
+            resonance: 800,            
+            modulationIndex: 20,       
+            envelope: { decay: 0.4, },                         
+            volume: -15
+    });
+
+    bell.toDestination();
+    const bellPart = new Tone.Sequence(((time, freq) => {        
+      bell.triggerAttack(freq, time, Math.random()*0.5 + 0.5); 
+            }), [[300, null, 200],                                       
+                [null, 200, 200],                                        
+                [null, 200, null],                                       
+                [200, null, 200]                                         
+            ], "4n");
+    let model = {
+        conga,
+        congaPart,
+        bell,
+        bellPart,
+    }
+    return model;
+}
